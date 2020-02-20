f9d800eb46b850654ad83991c3e028b59b06ddc6
tfpoivic <tfpoivic@gmail.com>
2020-02-20 17:05:07 +0800

    Initial Commit

---
+++ itfood_del/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  itfood_del
+//
+//  Created by 莊宇軒 on 2020/2/20.
+//  Copyright © 2020 dp103g3. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ itfood_delTests/itfood_delTests.swift
@@ -0,0 +1,34 @@
+//
+//  itfood_delTests.swift
+//  itfood_delTests
+//
+//  Created by 莊宇軒 on 2020/2/20.
+//  Copyright © 2020 dp103g3. All rights reserved.
+//
+
+import XCTest
+@testable import itfood_del
+
+class itfood_delTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
